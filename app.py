18f6ae1ef2a49a7181f220395760602d49491206
Emmanuel A <emmanuelxxix@gmail.com>
2023-01-24 17:52:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,34 @@
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session, url_for
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+from werkzeug.utils import secure_filename
+from passlib.apps import custom_app_context as pwd_context
+from tempfile import gettempdir
+from datetime import datetime
+from flask_mail import Mail, Message
+
+app = Flask(__name__)
+
+app.config["SESSION_FILE_DIR"] = gettempdir()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+app.config['MAIL_SERVER']= 'smtp.mailtrap.io'
+app.config['MAIL_PORT'] = 2525
+app.config['MAIL_USERNAME'] = '0a61b7f4f7d142'
+app.config['MAIL_PASSWORD'] = '9b96ccbd6ad93a'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['ALLOWED_EXTENSIONS'] = ALLOWED_EXTENSIONS
+
+mail = Mail(app)
+Session(app)
+
+db = SQL("sqlite:///database.db")
+
 
